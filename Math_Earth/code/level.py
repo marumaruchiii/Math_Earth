8987cdc71091f06c3de81e16ef95f005c2692624
maruuuu <89464581+marumaruchiii@users.noreply.github.com>
2024-01-08 23:21:20 +0800

    Fix bugs.

--- Math_Earth/code/level.py
+++ Math_Earth/code/level.py
@@ -41,14 +41,14 @@ class Level:
 
 	def create_map(self):
 		layouts = {
-			'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
-			'grass': import_csv_layout('../map/map_Grass.csv'),
-			'object': import_csv_layout('../map/map_Objects.csv'),
-			'entities': import_csv_layout('../map/map_Entities.csv')
+			'boundary': import_csv_layout('./map/map_FloorBlocks.csv'),
+			'grass': import_csv_layout('./map/map_Grass.csv'),
+			'object': import_csv_layout('./map/map_Objects.csv'),
+			'entities': import_csv_layout('./map/map_Entities.csv')
 		}
 		graphics = {
-			'grass': import_folder('../graphics/Grass'),
-			'objects': import_folder('../graphics/objects')
+			'grass': import_folder('./graphics/Grass'),
+			'objects': import_folder('./graphics/objects')
 		}
 
 		for style,layout in layouts.items():
@@ -82,7 +82,8 @@ class Level:
 									self.create_magic)
 							else:
 								if col == '390': monster_name = 'bamboo'
-								elif col == '391': monster_name = 'spirit'
+								elif col == '391': monster_name = 'spirit' #spirit
+								elif col == '393': monster_name = 'boxx' #boxx 393
 								elif col == '392': monster_name ='raccoon'
 								else: monster_name = 'squid'
 								Enemy(
@@ -167,7 +168,7 @@ class YSortCameraGroup(pygame.sprite.Group):
 		self.offset = pygame.math.Vector2()
 
 		# creating the floor
-		self.floor_surf = pygame.image.load('../graphics/tilemap/ground.png').convert()
+		self.floor_surf = pygame.image.load('./graphics/tilemap/ground.png').convert()
 		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
 	def custom_draw(self,player):
