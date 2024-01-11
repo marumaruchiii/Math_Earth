bd654821ce0a7645969e6445727bc97233948698
maruuuu <89464581+marumaruchiii@users.noreply.github.com>
2024-01-12 01:30:55 +0800

    Move scroll bar to UI.

--- Math_Earth/code/upgrade.py
+++ Math_Earth/code/upgrade.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.font import Font
+from ui import SettingScrollBar
 from settings import *
 
 class Upgrade:
@@ -168,62 +169,3 @@ class GameOption:
 			value = self.option[(list(self.option.keys())[index])]
 			max_value = self.max_values[index]
 			item.display(self.layer,self.selection_index,name,value,max_value,0)
-
-class SettingScrollBar:
-	def __init__(self,l,t,w,h,index,font):
-		self.rect = pygame.Rect(l,t,w,h)
-		self.index = index
-		self.ui_font = font
-
-	def display_names(self,surface,name,cost,selected):
-		color = TEXT_COLOR_SELECTED if selected else TEXT_COLOR
-
-		# title
-		title_surf = self.ui_font.render(name,False,color)
-		title_rect = title_surf.get_rect(midtop = self.rect.midtop + pygame.math.Vector2(0,20))
-
-		# cost 
-		cost_surf = self.ui_font.render(f'{int(cost)}',False,color)
-		cost_rect = cost_surf.get_rect(midbottom = self.rect.midbottom - pygame.math.Vector2(0,20))
-
-		# draw 
-		surface.blit(title_surf,title_rect)
-		surface.blit(cost_surf,cost_rect)
-
-	def display_bar(self,surface,value,max_value,selected):
-
-		# drawing setup
-		top = self.rect.midtop + pygame.math.Vector2(0,60)
-		bottom = self.rect.midbottom - pygame.math.Vector2(0,60)
-		color = BAR_COLOR_SELECTED if selected else BAR_COLOR
-
-		# bar setup
-		full_height = bottom[1] - top[1]
-		relative_number = (value / max_value) * full_height
-		value_rect = pygame.Rect(top[0] - 15,bottom[1] - relative_number,30,10)
-
-		# draw elements
-		pygame.draw.line(surface,color,top,bottom,5)
-		pygame.draw.rect(surface,color,value_rect)
-
-	def player_status_setting(self,player):
-		upgrade_attribute = list(player.stats.keys())[self.index]
-
-		if player.exp >= player.upgrade_cost[upgrade_attribute] and player.stats[upgrade_attribute] < player.max_stats[upgrade_attribute]:
-			player.exp -= player.upgrade_cost[upgrade_attribute]
-			player.stats[upgrade_attribute] *= 1.2
-			player.upgrade_cost[upgrade_attribute] *= 1.4
-
-		if player.stats[upgrade_attribute] > player.max_stats[upgrade_attribute]:
-			player.stats[upgrade_attribute] = player.max_stats[upgrade_attribute]
-
-	def display(self,surface,selection_num,name,value,max_value,cost):
-		if self.index == selection_num:
-			pygame.draw.rect(surface,UPGRADE_BG_COLOR_SELECTED,self.rect)
-			pygame.draw.rect(surface,UI_BORDER_COLOR,self.rect,4)
-		else:
-			pygame.draw.rect(surface,UI_BG_COLOR,self.rect)
-			pygame.draw.rect(surface,UI_BORDER_COLOR,self.rect,4)
-	
-		self.display_names(surface,name,cost,self.index == selection_num)
-		self.display_bar(surface,value,max_value,self.index == selection_num)
